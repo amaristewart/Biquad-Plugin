4d33435a71606766d5a3341a2977a95d1764b342
amaristewart <151667406+amaristewart@users.noreply.github.com>
2024-11-28 00:05:02 -0500

    Update cpp and juce files

---
+++ cpp_juce/codegen/lib/biquad/interface/_coder_biquad_info.cpp
@@ -0,0 +1,194 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// _coder_biquad_info.cpp
+//
+// Code generation for function 'onParamChangeCImpl'
+//
+
+// Include files
+#include "_coder_biquad_info.h"
+#include "emlrt.h"
+#include "tmwtypes.h"
+
+// Function Declarations
+static const mxArray *emlrtMexFcnResolvedFunctionsInfo();
+
+// Function Definitions
+static const mxArray *emlrtMexFcnResolvedFunctionsInfo()
+{
+  const mxArray *nameCaptureInfo;
+  const char_T *data[12]{
+      "789ced5c4d6f1b45185ea3065aa416b7888a3f4136b143621f38248ebf5adbd9c44e94a6"
+      "aab2b3eb89b366bfbc1f898dc42f400271e00f70e2d82347b870449c"
+      "1027d42312570e150784edf524f1a4d375b2938d3d9eb96c264fe67dde79c7fbf8c9ec87"
+      "9028571382203c108296d182e3fd513f393abe238c371c4f8c8e1f62",
+      "7dd416843b63e310fecde8a85aa607bb5ed0318101cf46362d433381e9357a36141ce85a"
+      "fa096c0e91234d870dcd80f58b9ddaa067142e40679d0134f839770c"
+      "d5cfebbe2138c7ee7986fac5ce593dfe20ccf7ce84f53825d42389e1cff32f44dbd14e80"
+      "07c513e0884796de848e2baa6d3175f445d75cc976d36d45d15db595",
+      "3296cd767bd959eab796293644cf5601c82a10ae1c66b24aea7065098243259b5e3a5c5d"
+      "c9a829a03695b5a38c6899127080913b06660be6ca86ad2f1a67f3ec"
+      "469ce7c3907922bc053d49f75b9a59365d0f982a5a6794877ccd3cf046ca03355aeb7b12"
+      "c287f018d6f75269fbcb1b5ad70f269c277e3cfffbbbc3e357af8bad",
+      "c1312ebeefed178b71f2a1765b7c51cfcfc704be2486fb293b732aed6f29db40cf14f79e"
+      "747aebbd5ce13c0f2984272c0f81d08f2b7e6c7a9ebbf1f3bd1f4feb"
+      "7f15aefb4dcd0a4efc0b7a2e479ce7bbc4790688a2757cd0bc3dddfee19a7c287e3e840f"
+      "e18375dc75072b070ce068ae074f81e3899b96ea1bd0f45cb1a87925",
+      "5f113786f5f8245807d1009e0e143128d2408663d38dafff895787bffbb7f2284e3ed458"
+      "d7e135a7d9d8f0d3cff6f672bbbadf7e52aeb64a529eebf0acf9aea8"
+      "9f974721f344f8e52f83e0f7b3eaab67fd7b76567df567bfffc27db530f9e7f423025f12"
+      "c3ad67dd9d4e2dbbbbbff3b422299b5b05bdbb5d15d8d1f359f56371",
+      "ed7b0cb75ea0079dba6159de317446bf8fcbaf372d5fd1e139dfcb887c12d617b0bf4378"
+      "a4f5ba54b445232e9df8f3d77875f7af4e7e214e3ed466557727f5d1"
+      "fb4f77da5eb6baeeb86e25b3596a178a6bb5d50d767457268cbf295f675f930fc57f80f5"
+      "713e84bbd06b00a76fadf780eec3e9d3eb9f22e62187e481f0e75b94",
+      "0534315ed979d11bd6f9e2d2d3dd83dade6ab1bed9ae953abbed564f0269492db2a3a7bf"
+      "11c64f5a478b103f89e131ec4b38b07faaf3eb7c419b97fd2699308f"
+      "59dd8fe0d7f982c6aff35d2dbe4c187f53fa725d3e147f81c81720433117e87d4e68ed2b"
+      "47dd47a884e481f0683e186dfc245019e7464f58e7e3be974e7c86fc",
+      "91ed582a74ddbce9393dc9d24c8fe6f538ee7fdfccc7fdefcdf071ff1b34ee7faf165f26"
+      "8c9f56fffb1e912f4046a2ce9cffad85e481704afe1795716ef48475"
+      "3eee7fe9c49709e3a7f53adafb217c08f7ed66df8a1d96aa8571fe69d1cf1f23e6b11d92"
+      "07c229e9275eceb9d119d6f9b88ed2892f13c64fabefbc8bf5cff902",
+      "a4af13f6e0121a6bf71f1c84e481f01bb8ff6054d1f1f9c8847c59d117d6f9b87ed2892f"
+      "13c633e2432bdc87d2f4a115ee4319e3e33a4a27be4c18cf888e9624"
+      "aea334ff9f97b88eb2c5c775944e7c99309e111dad701da5ea47b98e32c6c775944e7c99"
+      "307e5a75f45e081fc29181aa63fcd3a2a32f23e621617d3c0f8453f6",
+      "a3f579d117d6f9b87ed2893fabef1788aac3f7b13e9e37c24737f66c8c5ecb45edfa58d8"
+      "7b055ccff155ef169e07c84758a7b1620d972b367d48ff1deffda3ff"
+      "fd7cfc304e3ed458d7db757fcb59f9f4a8d1e9e6eab503582f3697fd76891dbd7d45183f"
+      "691d7b84f8490c8fe17e71d581fde8976f191f9b2f7f2ee06a7c283e",
+      "7f2e803f1710071f7f2e804efc5784f193d6f14b42fc2486c773be57fae14db55736ebc0"
+      "b075e8a2b71d705de7ba3ed93cb9ae078debfadb79c2f210087d56f7"
+      "97a3aedbc7217c087f83c8974d2f9d9ab67de6d711f3f836240f840ff7996fe185b709d2"
+      "4208e3759009f36445af58e7e3fbd5d1e2ff0f7ff22923",
+      ""};
+  nameCaptureInfo = nullptr;
+  emlrtNameCaptureMxArrayR2016a(&data[0], 26960U, &nameCaptureInfo);
+  return nameCaptureInfo;
+}
+
+mxArray *emlrtMexFcnProperties()
+{
+  mxArray *xEntryPoints;
+  mxArray *xInputs;
+  mxArray *xResult;
+  const char_T *propFieldName[9]{"Version",
+                                 "ResolvedFunctions",
+                                 "Checksum",
+                                 "EntryPoints",
+                                 "CoverageInfo",
+                                 "IsPolymorphic",
+                                 "PropertyList",
+                                 "UUID",
+                                 "ClassEntryPointIsHandle"};
+  const char_T *epFieldName[8]{
+      "QualifiedName",    "NumberOfInputs", "NumberOfOutputs", "ConstantInputs",
+      "ResolvedFilePath", "TimeStamp",      "Constructor",     "Visible"};
+  xEntryPoints =
+      emlrtCreateStructMatrix(1, 5, 8, (const char_T **)&epFieldName[0]);
+  xInputs = emlrtCreateLogicalMatrix(1, 2);
+  emlrtSetField(xEntryPoints, 0, "QualifiedName",
+                emlrtMxCreateString("onParamChangeCImpl"));
+  emlrtSetField(xEntryPoints, 0, "NumberOfInputs",
+                emlrtMxCreateDoubleScalar(2.0));
+  emlrtSetField(xEntryPoints, 0, "NumberOfOutputs",
+                emlrtMxCreateDoubleScalar(0.0));
+  emlrtSetField(xEntryPoints, 0, "ConstantInputs", xInputs);
+  emlrtSetField(
+      xEntryPoints, 0, "ResolvedFilePath",
+      emlrtMxCreateString(
+          "/private/var/folders/cj/2fzxn49x3jbblscg2m1njj1r0000gn/T/"
+          "tpcaa9bee4_89b2_40ea_b930_648c2acdb7f8/onParamChangeCImpl.m"));
+  emlrtSetField(xEntryPoints, 0, "TimeStamp",
+                emlrtMxCreateDoubleScalar(739583.99920138891));
+  emlrtSetField(xEntryPoints, 0, "Constructor",
+                emlrtMxCreateLogicalScalar(false));
+  emlrtSetField(xEntryPoints, 0, "Visible", emlrtMxCreateLogicalScalar(true));
+  xInputs = emlrtCreateLogicalMatrix(1, 1);
+  emlrtSetField(xEntryPoints, 1, "QualifiedName",
+                emlrtMxCreateString("resetCImpl"));
+  emlrtSetField(xEntryPoints, 1, "NumberOfInputs",
+                emlrtMxCreateDoubleScalar(1.0));
+  emlrtSetField(xEntryPoints, 1, "NumberOfOutputs",
+                emlrtMxCreateDoubleScalar(0.0));
+  emlrtSetField(xEntryPoints, 1, "ConstantInputs", xInputs);
+  emlrtSetField(xEntryPoints, 1, "ResolvedFilePath",
+                emlrtMxCreateString(
+                    "/private/var/folders/cj/2fzxn49x3jbblscg2m1njj1r0000gn/T/"
+                    "tpcaa9bee4_89b2_40ea_b930_648c2acdb7f8/resetCImpl.m"));
+  emlrtSetField(xEntryPoints, 1, "TimeStamp",
+                emlrtMxCreateDoubleScalar(739583.99920138891));
+  emlrtSetField(xEntryPoints, 1, "Constructor",
+                emlrtMxCreateLogicalScalar(false));
+  emlrtSetField(xEntryPoints, 1, "Visible", emlrtMxCreateLogicalScalar(true));
+  xInputs = emlrtCreateLogicalMatrix(1, 3);
+  emlrtSetField(xEntryPoints, 2, "QualifiedName",
+                emlrtMxCreateString("processEntryPoint"));
+  emlrtSetField(xEntryPoints, 2, "NumberOfInputs",
+                emlrtMxCreateDoubleScalar(3.0));
+  emlrtSetField(xEntryPoints, 2, "NumberOfOutputs",
+                emlrtMxCreateDoubleScalar(2.0));
+  emlrtSetField(xEntryPoints, 2, "ConstantInputs", xInputs);
+  emlrtSetField(
+      xEntryPoints, 2, "ResolvedFilePath",
+      emlrtMxCreateString(
+          "/private/var/folders/cj/2fzxn49x3jbblscg2m1njj1r0000gn/T/"
+          "tpcaa9bee4_89b2_40ea_b930_648c2acdb7f8/processEntryPoint.m"));
+  emlrtSetField(xEntryPoints, 2, "TimeStamp",
+                emlrtMxCreateDoubleScalar(739583.99920138891));
+  emlrtSetField(xEntryPoints, 2, "Constructor",
+                emlrtMxCreateLogicalScalar(false));
+  emlrtSetField(xEntryPoints, 2, "Visible", emlrtMxCreateLogicalScalar(true));
+  xInputs = emlrtCreateLogicalMatrix(1, 1);
+  emlrtSetField(xEntryPoints, 3, "QualifiedName",
+                emlrtMxCreateString("createPluginInstance"));
+  emlrtSetField(xEntryPoints, 3, "NumberOfInputs",
+                emlrtMxCreateDoubleScalar(1.0));
+  emlrtSetField(xEntryPoints, 3, "NumberOfOutputs",
+                emlrtMxCreateDoubleScalar(0.0));
+  emlrtSetField(xEntryPoints, 3, "ConstantInputs", xInputs);
+  emlrtSetField(
+      xEntryPoints, 3, "ResolvedFilePath",
+      emlrtMxCreateString(
+          "/private/var/folders/cj/2fzxn49x3jbblscg2m1njj1r0000gn/T/"
+          "tpcaa9bee4_89b2_40ea_b930_648c2acdb7f8/createPluginInstance.m"));
+  emlrtSetField(xEntryPoints, 3, "TimeStamp",
+                emlrtMxCreateDoubleScalar(739583.99920138891));
+  emlrtSetField(xEntryPoints, 3, "Constructor",
+                emlrtMxCreateLogicalScalar(false));
+  emlrtSetField(xEntryPoints, 3, "Visible", emlrtMxCreateLogicalScalar(true));
+  xInputs = emlrtCreateLogicalMatrix(1, 0);
+  emlrtSetField(xEntryPoints, 4, "QualifiedName",
+                emlrtMxCreateString("getLatencyInSamplesCImpl"));
+  emlrtSetField(xEntryPoints, 4, "NumberOfInputs",
+                emlrtMxCreateDoubleScalar(0.0));
+  emlrtSetField(xEntryPoints, 4, "NumberOfOutputs",
+                emlrtMxCreateDoubleScalar(1.0));
+  emlrtSetField(xEntryPoints, 4, "ConstantInputs", xInputs);
+  emlrtSetField(
+      xEntryPoints, 4, "ResolvedFilePath",
+      emlrtMxCreateString(
+          "/private/var/folders/cj/2fzxn49x3jbblscg2m1njj1r0000gn/T/"
+          "tpcaa9bee4_89b2_40ea_b930_648c2acdb7f8/getLatencyInSamplesCImpl.m"));
+  emlrtSetField(xEntryPoints, 4, "TimeStamp",
+                emlrtMxCreateDoubleScalar(739583.99920138891));
+  emlrtSetField(xEntryPoints, 4, "Constructor",
+                emlrtMxCreateLogicalScalar(false));
+  emlrtSetField(xEntryPoints, 4, "Visible", emlrtMxCreateLogicalScalar(true));
+  xResult =
+      emlrtCreateStructMatrix(1, 1, 9, (const char_T **)&propFieldName[0]);
+  emlrtSetField(xResult, 0, "Version",
+                emlrtMxCreateString("24.2.0.2740171 (R2024b) Update 1"));
+  emlrtSetField(xResult, 0, "ResolvedFunctions",
+                (mxArray *)emlrtMexFcnResolvedFunctionsInfo());
+  emlrtSetField(xResult, 0, "Checksum",
+                emlrtMxCreateString("CB1fHBl10Yk3BktsJHLKkD"));
+  emlrtSetField(xResult, 0, "EntryPoints", xEntryPoints);
+  return xResult;
+}
+
+// End of code generation (_coder_biquad_info.cpp)
